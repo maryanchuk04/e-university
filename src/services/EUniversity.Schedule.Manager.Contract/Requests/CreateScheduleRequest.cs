66984de33731f7e8a5fba8f4b9a8e325192f0950
Maksym Marianchuk <c-maksym.marianchuk@skykick.com>
2024-06-22 05:33:36 +0300

    Temp Changes

--- src/services/EUniversity.Schedule.Manager.Contract/Requests/CreateScheduleRequest.cs
+++ src/services/EUniversity.Schedule.Manager.Contract/Requests/CreateScheduleRequest.cs
@@ -1,5 +1,7 @@
-﻿namespace EUniversity.Schedule.Manager.Contract.Requests;
+namespace EUniversity.Schedule.Manager.Contract.Requests;
 
 public class CreateScheduleRequest
 {
+    public Guid GroupId { get; set; }
+    public Guid UserId { get; set; }
 }
